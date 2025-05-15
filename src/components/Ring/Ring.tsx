19005c866084f4f91f029b89d88ff6e456d46e99
barrelrolla <chetkara@gmail.com>
2025-05-15 10:33:21 +0300

    updated ring animation

--- src/components/Ring/Ring.tsx
+++ src/components/Ring/Ring.tsx
@@ -1,5 +1,5 @@
 export default function Ring() {
   return (
-    <div className="absolute w-[200px] h-[200px] sm:w-[350px] sm:h-[350px] xl:w-[500px] xl:h-[500px] rounded-full self-center justify-self-center animate-slow-spin opacity-50 hero-ring"></div>
+    <div className="absolute w-[200px] h-[200px] sm:w-[350px] sm:h-[350px] xl:w-[500px] xl:h-[500px] rounded-full self-center justify-self-center animate-slow-spin hero-ring opacity-80"></div>
   );
 }
