01e048674143d8942ec407d1b145ffd08acae53f
barrelrolla <chetkara@gmail.com>
2025-05-19 10:08:59 +0300

    animated navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,22 +3,31 @@ import {
   Navbar as BarrelrollaNavbar,
   Button,
   NavbarBrand,
-  NavbarCollapse,
   NavbarLink,
   NavbarToggle,
 } from "@barrelrolla/react-components-library";
 import { navLinks } from "../../data/navLinks";
+import { motion } from "motion/react";
+import { MotionCollapse } from "../motion/motion";
 
 export default function Navbar() {
   const path = useLocation().pathname;
   return (
     <BarrelrollaNavbar collapseAt="md">
-      <div className="md:w-[130px]">
+      <motion.div
+        initial={{ opacity: 0, translateX: -100 }}
+        animate={{ opacity: 1, translateX: 0 }}
+        className="md:w-[130px]"
+      >
         <NavbarBrand as={Link} className="w-min font-medium" to={"/"}>
           Julian
         </NavbarBrand>
-      </div>
-      <NavbarCollapse className="md:space-x-6">
+      </motion.div>
+      <MotionCollapse
+        initial={{ opacity: 0, translateY: -50 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        className="md:space-x-6"
+      >
         {navLinks.map((link) => (
           <NavbarLink
             key={link.path}
@@ -29,8 +38,12 @@ export default function Navbar() {
             {link.path}
           </NavbarLink>
         ))}
-      </NavbarCollapse>
-      <div className="flex flex-wrap items-center gap-4 md:w-[130px]">
+      </MotionCollapse>
+      <motion.div
+        initial={{ opacity: 0, translateX: 100 }}
+        animate={{ opacity: 1, translateX: 0 }}
+        className="flex flex-wrap items-center gap-4 md:w-[130px]"
+      >
         <Button
           variant="outline"
           as={"a"}
@@ -43,7 +56,7 @@ export default function Navbar() {
           Download CV
         </Button>
         <NavbarToggle />
-      </div>
+      </motion.div>
     </BarrelrollaNavbar>
   );
 }
--- src/components/motion/motion.ts
+++ src/components/motion/motion.ts
@@ -1,5 +1,10 @@
-import { HeroSection, HeroTitle } from "@barrelrolla/react-components-library";
+import {
+  HeroSection,
+  HeroTitle,
+  NavbarCollapse,
+} from "@barrelrolla/react-components-library";
 import { motion } from "motion/react";
 
 export const MotionHeroTitle = motion.create(HeroTitle);
 export const MotionHeroSection = motion.create(HeroSection);
+export const MotionCollapse = motion.create(NavbarCollapse);
